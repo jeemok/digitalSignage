cb02e1733a3ac9e045e782e603a9169f59ab5226
Jee Mok <jee.ict@hotmail.com>
2018-12-04 18:23:17 +0900

    Update uploadImage.php

--- functions/uploadImage.php
+++ functions/uploadImage.php
@@ -1,6 +1,6 @@
 <?php
   $rootPath = dirname(dirname(__FILE__));
-  $targetDir = $rootPath . "\screens\\" . $_POST["screen"] . "\images\\";
+  $targetDir = $rootPath . "/screens/" . $_POST["screen"] . "/images/";
   $targetFile = $targetDir . basename($_FILES["fileToUpload"]["name"]);
   $imageFileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
 
@@ -47,26 +47,24 @@
       echo '</body>';
     }
     // if everything is ok, try to upload file
+    else if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $targetFile)) {
+      // Redirect back to main page
+      header("Location: ../main.php");
+
+      echo '<body style="padding: 100px; text-align: center; font-family: Arial;">';
+      echo "<img src=\"images/success.png\" style=\"max-width: 500px;\" />";
+      echo "<p>The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.</p>";
+      echo '</body>';
+    }
     else {
-      if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $targetFile)) {
-        // Redirect back to main page
-        header("Location: ../main.php");
+      // Redirect back to index.php after 2 seconds
+      header("Refresh: 2; URL = ../main.php");
 
-        echo '<body style="padding: 100px; text-align: center; font-family: Arial;">';
-        echo "<img src=\"images/success.png\" style=\"max-width: 500px;\" />";
-        echo "<p>The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.</p>";
-        echo '</body>';
-      }
-      else {
-        // Redirect back to index.php after 2 seconds
-        header("Refresh: 2; URL = ../main.php");
-
-        echo '<body style="padding: 100px; text-align: center; font-family: Arial;">';
-        echo "<img src=\"images/failed.png\" style=\"max-width: 200px;\" />";
-        echo "<p>Sorry, there was an error uploading your file.</p>";
-        echo "<p>Page will auto redirect in 2 seconds.</p>";
-        echo '</body>';
-      }
+      echo '<body style="padding: 100px; text-align: center; font-family: Arial;">';
+      echo "<img src=\"images/failed.png\" style=\"max-width: 200px;\" />";
+      echo "<p>Sorry, there was an error uploading your file.</p>";
+      echo "<p>Page will auto redirect in 2 seconds.</p>";
+      echo '</body>';
     }
   }
 ?>
