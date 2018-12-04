6cefa409a44022ddf975a9a3f980f0a8ce60f155
Jee Mok <jee.ict@hotmail.com>
2018-12-04 17:50:05 +0900

    Moved header to above

--- functions/logout.php
+++ functions/logout.php
@@ -5,11 +5,11 @@
    unset($_SESSION["displayName"]);
    unset($_SESSION["username"]);
 
+   // Redirect back to index.php
+   header("Location: ../index.php");
+
    echo '<body style="padding: 100px; text-align: center; font-family: Arial;">';
    echo '<img src="/functions/images/success.png" style="max-width: 500px;" />';
    echo "<p>You have logout. Your current session has been cleaned.</p>";
    echo '</body>';
-
-   // Redirect back to index.php
-   header("Location: ../index.php");
 ?>
