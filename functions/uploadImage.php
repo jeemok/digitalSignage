140f987d0af1e0eda5d0aa7ce496050ca7fe8654
Jee Mok <jee.ict@hotmail.com>
2018-12-04 18:04:14 +0900

    Update uploadImage.php

--- functions/uploadImage.php
+++ functions/uploadImage.php
@@ -8,6 +8,9 @@
   if(isset($_POST["submit"])) {
     // Check if image file is an actual image
     if(getimagesize($_FILES["fileToUpload"]["tmp_name"]) === false) {
+      // Redirect back to index.php after 2 seconds
+      header("Refresh: 2; URL = ../main.php");
+
       echo '<body style="padding: 100px; text-align: center; font-family: Arial;">';
       echo "<img src=\"images/failed.png\" style=\"max-width: 200px;\" />";
       echo "<p>File is not an image.</p>";
@@ -15,20 +18,29 @@
     }
     // Check if file already exists
     else if (file_exists($targetFile)) {
+      // Redirect back to index.php after 2 seconds
+      header("Refresh: 2; URL = ../main.php");
+
       echo '<body style="padding: 100px; text-align: center; font-family: Arial;">';
       echo "<img src=\"images/failed.png\" style=\"max-width: 200px;\" />";
       echo "<p>Sorry, file already exists.</p>";
       echo '</body>';
     }
     // Check file size
-    else if ($_FILES["fileToUpload"]["size"] > 500000) {
+    else if (round($_FILES["fileToUpload"]["size"] / 1024 / 1024, 1) > 2) { // count in megabytes
+      // Redirect back to index.php after 2 seconds
+      header("Refresh: 2; URL = ../main.php");
+
       echo '<body style="padding: 100px; text-align: center; font-family: Arial;">';
       echo "<img src=\"images/failed.png\" style=\"max-width: 200px;\" />";
-      echo "<p>Sorry, your file is too large.</p>";
+      echo "<p>Sorry, your file is too large. Image has to be less than 2 MB.</p>";
       echo '</body>';
     }
     // Check file formats
     else if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif" ) {
+      // Redirect back to index.php after 2 seconds
+      header("Refresh: 2; URL = ../main.php");
+
       echo '<body style="padding: 100px; text-align: center; font-family: Arial;">';
       echo "<img src=\"images/failed.png\" style=\"max-width: 200px;\" />";
       echo "<p>Sorry, only JPG, JPEG, PNG & GIF files are allowed.</p>";
@@ -39,7 +51,7 @@
       if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $targetFile)) {
         // Redirect back to main page
         header("Location: ../main.php");
-        
+
         echo '<body style="padding: 100px; text-align: center; font-family: Arial;">';
         echo "<img src=\"images/success.png\" style=\"max-width: 500px;\" />";
         echo "<p>The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.</p>";
