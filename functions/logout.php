c14d540aa2f16f9265da49a4f635530c8f6ed646
Jee Mok <jee.ict@hotmail.com>
2018-12-04 17:49:00 +0900

    Fixed error

--- functions/logout.php
+++ functions/logout.php
@@ -1,15 +1,15 @@
-<body style="padding: 100px; text-align: center; font-family: Arial;">
-  <?php
-     session_start();
-     unset($_SESSION["valid"]);
-     unset($_SESSION["timestamp"]);
-     unset($_SESSION["displayName"]);
-     unset($_SESSION["username"]);
+<?php
+   session_start();
+   unset($_SESSION["valid"]);
+   unset($_SESSION["timestamp"]);
+   unset($_SESSION["displayName"]);
+   unset($_SESSION["username"]);
 
-     echo '<img src="/functions/images/success.png" style="max-width: 500px;" />';
-     echo "<p>You have logout. Your current session has been cleaned.</p>";
+   echo '<body style="padding: 100px; text-align: center; font-family: Arial;">';
+   echo '<img src="/functions/images/success.png" style="max-width: 500px;" />';
+   echo "<p>You have logout. Your current session has been cleaned.</p>";
+   echo '</body>';
 
-     // Redirect back to index.php
-     header("Location: ../index.php");
-  ?>
-</body>
+   // Redirect back to index.php
+   header("Location: ../index.php");
+?>
